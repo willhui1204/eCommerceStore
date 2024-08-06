54dbafc5d673d7115baec754f959a969707bdcf0
kwin256 <kwin256wei@gmail.com>
2024-08-05 22:29:55 -0600

    github sign try

--- app/page.js
+++ app/page.js
@@ -2,20 +2,37 @@
 // Import the useUserAuth hook
 import { useUserAuth } from "./user/_utils/auth-context";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
   const { user, gitHubSignIn, firebaseSignOut, register, login } =
     useUserAuth();
+  const router = useRouter();
+
+  const handleGitHubSignIn = () => {
+    // 确保弹窗是由用户点击触发的
+    gitHubSignIn()
+      .then(() => {
+        router.push("/user");
+      })
+      .catch((error) => {
+        console.error("Error during GitHub sign in:", error);
+      });
+  };
   if (!user) {
     return (
       <div className="">
-        <button className="btn m-3" onClick={gitHubSignIn}>
+        <button className="btn m-3" onClick={handleGitHubSignIn}>
           Sign in with GitHub
         </button>
 
-        <Link className="btn m-3" href="./user/login">Login</Link>
+        <Link className="btn m-3" href="./user/login">
+          Login
+        </Link>
 
-        <Link className="btn m-3" href="./user/register">Register</Link>  
+        <Link className="btn m-3" href="./user/register">
+          Register
+        </Link>
       </div>
     );
   }
@@ -24,7 +41,9 @@ export default function Page() {
   // Return null as the component does not need to render anything
   return (
     <>
-      <Link className="btn m-3" href="/user">Shopping Now</Link>
+      <Link className="btn m-3" href="/user">
+        Shopping Now
+      </Link>
       <button className=" btn " onClick={firebaseSignOut}>
         Sign out
       </button>
