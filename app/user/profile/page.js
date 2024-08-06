cb9a9a4f50017087e8199c430fa1bcf2b037db30
kwin256 <kwin256wei@gmail.com>
2024-08-05 18:59:54 -0600

    solve build error

--- app/user/profile/page.js
+++ app/user/profile/page.js
@@ -1,7 +1,17 @@
 "use client";
 import { useUserAuth } from "../_utils/auth-context";
+import Link from "next/link";
 
 export default function Page() {
     const { user } = useUserAuth();
+  if (!user) {
+    return (
+      <div className="">
+        <Link className="btn m-3" href="../user/login">Login</Link>
+        <Link className="btn m-3" href="../user/register">Register</Link>
+      </div>
+    );
+  }
+
   return (<h1>Welcome to profile, {user.uid}, {user.email}</h1>);
 }
